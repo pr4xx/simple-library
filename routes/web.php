39f72cca3533d6d3aa2813446fcdbdbcc891ad2b
Nico Zimmermann <nico.zimmermann@deutschebahn.com>
2018-11-21 22:41:40 +0100

    Add simple auth (login only)

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes([
+    'register' => false,
+    'reset' => false,
+    'verify' => false
+]);
+
+Route::middleware('auth')->group(function() {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+
+
