0563e4585ba1de77ab14d05e4a1a7c8fe54e3080
Nico Zimmermann <nico.zimmermann94@googlemail.com>
2018-12-18 22:16:09 +0100

    Update

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -44,15 +44,34 @@ class BookController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'signature' => ['required', Rule::unique('books')]
+            'signature' => ['required', Rule::unique('books')],
+            'author_name' => ['nullable', Rule::unique('authors', 'name')],
+            'origin_title' => ['nullable', Rule::unique('origins', 'title')]
         ]);
 
         $book = new Book();
         $book->signature = $request->get('signature');
         $book->title = $request->get('title');
         $book->original_title = $request->get('original_title');
-        $book->author_id = optional(Author::find($request->get('author_id')))->id;
-        $book->origin_id = optional(Origin::find($request->get('origin_id')))->id;
+
+        if($authorName = $request->get('author_name')) {
+            $author = new Author();
+            $author->name = $authorName;
+            $author->save();
+            $book->author_id = $author->id;
+        } else {
+            $book->author_id = optional(Author::find($request->get('author_id')))->id;
+        }
+
+        if($originTitle = $request->get('origin_title')) {
+            $origin = new Origin();
+            $origin->title = $originTitle;
+            $origin->save();
+            $book->origin_id = $origin->id;
+        } else {
+            $book->origin_id = optional(Origin::find($request->get('origin_id')))->id;
+        }
+
         $book->category_id = optional(Category::find($request->get('category_id')))->id;
         $book->save();
         $book->tags()->sync(Tag::findMany($request->get('tag_ids', []))->pluck('id')->toArray());
--- resources/views/books/form.blade.php
+++ resources/views/books/form.blade.php
@@ -37,41 +37,79 @@
     </div>
 </div>
 
-<div class="form-group{{ $errors->has('author_id') ? ' has-error' : '' }}">
+<div class="form-group{{ $errors->has('author_id') || $errors->has('author_name') ? ' has-error' : '' }}">
     <label for="author_id" class="col-sm-2 control-label">Autor*in</label>
     <div class="col-sm-10">
-        <select id="author_id" name="author_id" class="selectpicker form-control" data-live-search="true" title="Auswählen">
-            <option value="" style="font-style: italic;">Auswählen</option>
-            @foreach($authors as $author)
-                <option value="{{ $author->id }}"
-                        @if(old('author_id', optional($book)->author_id) == $author->id) selected @endif>
-                    {{ $author->name }}
-                </option>
-            @endforeach
-        </select>
+        <div id="author-select">
+            <select id="author_id" name="author_id" class="selectpicker form-control" data-live-search="true" title="Auswählen">
+                <option value="" style="font-style: italic;">Auswählen</option>
+                @foreach($authors as $author)
+                    <option value="{{ $author->id }}"
+                            @if(old('author_id', optional($book)->author_id) == $author->id) selected @endif>
+                        {{ $author->name }}
+                    </option>
+                @endforeach
+            </select>
+            @if(!optional($book)->id)
+                <span class="help-block">
+                    <button type="button" class="btn btn-xs btn-default" onclick="addAuthor(true);">Hinzufügen</button>
+                </span>
+            @endif
+        </div>
+        <div id="author-add" style="display: none;">
+            <input type="text" class="form-control" id="author_name" name="author_name" placeholder="Name"
+                   value="{{ old('author_name') }}">
+            <span class="help-block">
+                <button type="button" class="btn btn-xs btn-default" onclick="selectAuthor();">Auswählen</button>
+            </span>
+        </div>
         @if ($errors->has('author_id'))
             <span class="help-block">
                 <strong>{{ $errors->first('author_id') }}</strong>
             </span>
         @endif
+        @if ($errors->has('author_name'))
+            <span class="help-block">
+                <strong>{{ $errors->first('author_name') }}</strong>
+            </span>
+        @endif
     </div>
 </div>
 
-<div class="form-group{{ $errors->has('origin_id') ? ' has-error' : '' }}">
+<div class="form-group{{ $errors->has('origin_id') || $errors->has('origin_title') ? ' has-error' : '' }}">
     <label for="origin_id" class="col-sm-2 control-label">Ort</label>
     <div class="col-sm-10">
-        <select id="origin_id" name="origin_id" class="selectpicker form-control" data-live-search="true" title="Auswählen">
-            <option value="" style="font-style: italic;">Auswählen</option>
-            @foreach($origins as $origin)
-                <option value="{{ $origin->id }}"
-                        @if(old('origin_id', optional($book)->origin_id) == $origin->id) selected @endif>
-                    {{ $origin->title }}
-                </option>
-            @endforeach
-        </select>
+        <div id="origin-select">
+            <select id="origin_id" name="origin_id" class="selectpicker form-control" data-live-search="true" title="Auswählen">
+                <option value="" style="font-style: italic;">Auswählen</option>
+                @foreach($origins as $origin)
+                    <option value="{{ $origin->id }}"
+                            @if(old('origin_id', optional($book)->origin_id) == $origin->id) selected @endif>
+                        {{ $origin->title }}
+                    </option>
+                @endforeach
+            </select>
+            @if(!optional($book)->id)
+                <span class="help-block">
+                    <button type="button" class="btn btn-xs btn-default" onclick="addOrigin(true);">Hinzufügen</button>
+                </span>
+            @endif
+        </div>
+        <div id="origin-add" style="display: none;">
+            <input type="text" class="form-control" id="origin_title" name="origin_title" placeholder="Titel"
+                   value="{{ old('origin_title') }}">
+            <span class="help-block">
+                <button type="button" class="btn btn-xs btn-default" onclick="selectOrigin();">Auswählen</button>
+            </span>
+        </div>
         @if ($errors->has('origin_id'))
             <span class="help-block">
                 <strong>{{ $errors->first('origin_id') }}</strong>
+            </span>
+        @endif
+        @if ($errors->has('origin_title'))
+            <span class="help-block">
+                <strong>{{ $errors->first('origin_title') }}</strong>
             </span>
         @endif
     </div>
@@ -115,3 +153,53 @@
         @endif
     </div>
 </div>
+
+@push('scripts')
+    <script type="text/javascript">
+
+        // Origin functions
+        function addOrigin(focus) {
+            $('#origin-select').hide();
+            $('#origin-add').show();
+            $('#origin_id').selectpicker('val', '');
+            if(focus) {
+                $('#origin_title').focus();
+            }
+        }
+
+        function selectOrigin() {
+            $('#origin-select').show();
+            $('#origin-add').hide();
+            $('#origin_title').val('')
+        }
+        
+        // Author functions
+        function addAuthor(focus) {
+            $('#author-select').hide();
+            $('#author-add').show();
+            $('#author_id').selectpicker('val', '');
+            if(focus) {
+                $('#author_name').focus();
+            }
+        }
+
+        function selectAuthor() {
+            $('#author-select').show();
+            $('#author-add').hide();
+            $('#author_name').val('')
+        }
+        
+
+        $(document).ready(function () {
+
+            if($('#origin_title').val()) {
+                addOrigin();
+            }
+
+            if($('#author_name').val()) {
+                addAuthor();
+            }
+
+        });
+    </script>
+@endpush
